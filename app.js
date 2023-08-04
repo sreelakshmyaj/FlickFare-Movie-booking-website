a106dc6830b1f073d32f303a9d4a85078b16ebba
Sreel <sreelakshmyaj@gmail.com>
2023-08-04 10:07:19 +0530

    touch ups and modifications

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const info = [
         "rating": "8.8",
         "duration": "201",
         "genre": "Adventure, Animation, Comedy",
-        "cert": "U"
+        "cert": "U",
+        "video": "https://www.youtube.com/watch?v=hXzcyx9V0xw",
     },
     {
         "img": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/barbie-et00072466-1688969871.jpg",
@@ -16,7 +17,8 @@ const info = [
         "rating": "7.7",
         "duration": "114",
         "genre": "Adventure, Comedy, Fantasy",
-        "cert": "UA"
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=pBk4NYhWNMM",
     },
     {
         "img": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/oppenheimer-et00347867-1683533414.jpg",
@@ -25,7 +27,8 @@ const info = [
         "rating": "8.8",
         "duration": "190",
         "genre": "Biography, Drama, Historical",
-        "cert": "UA"
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=uYPbbksJxIg&t=1s",
     },
     
     {
@@ -35,7 +38,8 @@ const info = [
         "rating": "8.9",
         "duration": "144",
         "genre": "Action, Sci-Fi, Thriller",
-        "cert": "UA"
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=EXeTwQWrcwY",
     },
     
     {
@@ -45,7 +49,8 @@ const info = [
         "rating": "9.3",
         "duration": "140",
         "genre": "Action, Adventure, Animation",
-        "cert": "UA"
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=cqGjhVJWtEg",
     },
     
     {
@@ -55,7 +60,40 @@ const info = [
         "rating": "9.0",
         "duration": "171",
         "genre": "Action, Thriller",
-        "cert": "A"
+        "cert": "A",
+        "video": "https://www.youtube.com/watch?v=yjRHZEUamCc",
+    },
+    
+    {
+        "img": "https://assets-in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/the-super-mario-bros-movie-et00343051-1686571962.jpg",
+        "title": "The Super Mario Bros",
+        "date": "April 7",
+        "rating": "8.6",
+        "duration": "92",
+        "genre": "Adventure, Animation, Comedy, Family",
+        "cert": "U",
+        "video": "https://www.youtube.com/watch?v=TnGl01FkMMo",
+    },
+    
+    {
+        "img": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/mission-impossible-dead-reckoning--part-one-et00329481-1688284554.jpg",
+        "title": "Mission Impossible: Dead Reckoning",
+        "date": "July 12",
+        "rating": "9.1",
+        "duration": "165",
+        "genre": "Action, Adventure, Thriller",
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=avz06PDqDbM",
+    },
+    {
+        "img": "https://assets-in.bmscdn.com/iedb/movies/images/extra/vertical_logo/mobile/thumbnail/xxlarge/the-flash-et00047401-1689833276.jpg",
+        "title": "The Flash",
+        "date": "July 15",
+        "rating": "6.9",
+        "duration": "144",
+        "genre": "Action, Adventure, Fantast",
+        "cert": "UA",
+        "video": "https://www.youtube.com/watch?v=hebWYacbdvc",
     },
 ]
 
